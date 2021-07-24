e60f58c8365a11b3cf7fdfa5cce23236bc8b4fd9
Qing <qzou3@g.ucla.edu>
2021-07-23 23:58:48 -0700

    Added background color and figured out how to compare the current time with the calender time

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+
+var currentTimeHeaderFormat = moment().format('dddd MMMM Do')
+let day = moment().date()
+let year = moment().year()
+let month = moment().month()
+let second = moment().second()
+let minute = moment().minute()
+let hour = moment().hour()
+
+var currentTime = moment().year(year).month(month).date(day).hour(12).minute(minute).second(second)
+
+
+var momentTimes = []
+for (let i=0; i<9; i++){
+    let time = moment().year(year).month(month).date(day).hour(9 + i).minute(0).second(0)
+    momentTimes.push(time)
+}
+
+$("#currentDay").text(currentTimeHeaderFormat)
+let list = $("li")
+list.each(function(index, element){
+    let isBefore = momentTimes[index].isBefore(currentTime)
+    let isAfter = momentTimes[index].isAfter(currentTime)
+    let sameTime = momentTimes[index].hour() === currentTime.hour()
+    momentTimes[index]
+    if (isBefore) $(element).find("textarea").addClass('past');
+    if (isAfter)  $(element).find("textarea").addClass('future');
+    if (sameTime)  $(element).find("textarea").addClass('present');
+})
